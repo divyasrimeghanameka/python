f5469f458d9115285c7d2474dc554f22870669d9
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-07-30 11:39:23 +0530

    Create dlargeele3.py
    to find the largest element among the three numbers

---
+++ dlargeele3.py
@@ -0,0 +1,13 @@
+a=int(input("enter the value of a="))
+b=int(input("enter the value of b="))
+c=int(input("enter the value of c="))
+if a>b:
+  if a>c:
+    print(a,"is large")
+  else:
+    print(c,"is large")
+else:
+  if b>c:
+    print(b,"is large")
+  else:
+    print(c,"is large")
