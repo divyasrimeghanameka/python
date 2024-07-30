ca368b85ffe4819ed9015e663c870ca5808152ea
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-07-30 11:43:33 +0530

    Create dvowel2.py
    to check weather the alphabet is vowel or consonant

---
+++ dvowel2.py
@@ -0,0 +1,5 @@
+ch=input("enter the alphabet:")
+if ch=='a' or ch=='i' or ch=='o' or ch=='e' or ch=='u'or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+  print(ch,"is vowel")
+else:
+  print(ch,"is consonant")
