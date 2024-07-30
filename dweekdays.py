abf35ca4300126b6d2adb9a4838c8b9e67d12e52
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-07-30 11:50:00 +0530

    Create dweekdays.py
    to determine the days of the week

---
+++ dweekdays.py
@@ -0,0 +1,17 @@
+a=int(input("enter a value:"))
+if a==1:
+  print("MONDAY")
+elif a==2:
+  print("TUESDAY")
+elif a==3:
+  print("WEDNESDAY")
+elif a==4:
+  print("THURSDAY")
+elif a==5:
+  print("FRIDAY")
+elif a==6:
+  print("SATURDAY")
+elif a==7:
+  print("SUNDAY")
+else:
+  print("number is invalid")
