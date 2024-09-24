3eab59cef5096b52b2fe1e331112fa661f33fe34
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-09-24 10:02:43 +0530

    Update adding_key-value_pair_to_existing_dictionary.py

--- adding_key-value_pair_to_existing_dictionary.py
+++ adding_key-value_pair_to_existing_dictionary.py
@@ -2,5 +2,6 @@ def add_key_value(dictionary,key,value):
 	dictionary[key]=value
 	return dictionary
 my_dict={'a':10,'b':20,'c':30,'d':40}
+print("dictionary before adding a new key-value pair:",my_dict)
 updated_dict=add_key_value(my_dict,'e',50)
 print("dictionary after add a key-value pair: ",updated_dict)
