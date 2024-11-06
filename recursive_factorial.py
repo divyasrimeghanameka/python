2bffc95ff679ce2daed7559fc9719a90c50013f2
divyasrimeghanameka <23pa1a05e8@vishnu.edu.in>
2024-11-06 20:18:34 +0530

    Update recursive_factorial.py

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -4,7 +4,7 @@ def fact(n):
 	else:
 		return n*fact(n-1)
 n=int(input("enter a number:"))
-if n<=0:
+if n<0:
 	print("please enter a positive number")
 else:
 	print("factorial of ",n," is ",fact(n))
